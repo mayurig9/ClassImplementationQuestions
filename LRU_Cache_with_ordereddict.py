af4e64997d0f4b53d3111ec1208c5f95e38878da
MFA22SCM66G <mgorhe@hawk.iit.edu>
2024-09-11 19:54:05 -0700

    msg

--- LRU_Cache_with_ordereddict.py
+++ LRU_Cache_with_ordereddict.py
@@ -1,3 +1,30 @@
+from collections import OrderedDict
 class LRUCache:
-    def __init__(self,capacity):
+    def __init__(self, capacity: int):
+        self.cache =OrderedDict()
         self.capacity = capacity
+        
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        self.cache[key]=value
+        self.cache.move_to_end(key)
+    
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+
+
+ob = LRUCache(3)
+param_1 = ob.get(2)
+print(param_1)
+ob.put(1,"One")
+ob.put(2,"Two")
+ob.put(4,"Four")
+param5 = ob.get(2)
+param6 = ob.get(1)
+ob.put(3,"Three")
+print(ob.cache)
